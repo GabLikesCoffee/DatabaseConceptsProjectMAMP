d278ba48619bbd2ad8461a81f70586d76c13e8b9
Josh Abrams <josh.abrams111@gmail.com>
2023-03-14 17:52:24 -0400

    Update create.sql
    added email and university, added different insert

--- SQL Tester/create.sql
+++ SQL Tester/create.sql
@@ -1,10 +1,23 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 CREATE TABLE `Users` (
   `userId` char(40) DEFAULT NULL,
   `password` char(40) DEFAULT NULL,
-  `userLevel` char(40) DEFAULT NULL
+  `userLevel` char(40) DEFAULT NULL,
   `email` char(40) DEFAULT NULL,
   `university` char(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Users` (`userId`, `password`, `userLevel`, `email`, `university`) VALUES
+('TomB', 'Brady', 'student', `tomb@retired.com`, `michigan`),
+('AarR', 'Rodgers', 'student', `arod@jets.com`, `california`),
+('PeyM', 'Manning', 'student', `pman@retired.com`, `tennesee`),
+('MacJ', 'Jones', 'admin', `mac@pats.com`, `alabama`),
+('JoeB', 'Burrow', 'admin', `joey@bengals.com`, `lsu`),
+('PatM', 'Mahomes', 'superadmin', `patty@retired.com`, `texastech`);
+COMMIT;
 
 
 CREATE TABLE `Events` (
